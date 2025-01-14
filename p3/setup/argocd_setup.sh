2b60b714e8dd4ceaf6d4450eec4199ee1c885f92
ilahyani <lahyani799@gmail.com>
2025-01-14 22:33:50 +0100

    Argocd setup with Ansible Playbook

--- p3/setup/argocd_setup.sh
+++
@@ -1,5 +0,0 @@
-kubectl create namespace argocd
-kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml -n argocd
-kubectl wait --for=condition=Ready pods --all -n argocd --timeout=300s
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d > passwd.txt
-kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8080:80
