6767fc1785e825ab08c5099f003d4a7ebfac5e63
ilahyani <lahyani799@gmail.com>
2025-03-04 11:57:55 +0000

    setup script for the VM

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+sudo apt update
+
+## INSTALL VIRTUALBOX
+
+sudo apt install -y software-properties-common apt-transport-https wget
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo gpg --dearmor -o /usr/share/keyrings/oracle-virtualbox-2016.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] http://download.virtualbox.org/virtualbox/debian bookworm contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+sudo apt update
+sudo apt install -y virtualbox-7.1
+
+## INSTALL VAGRANT
+
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install vagrant
+
+## INSTALL ANSIBLE
+
+sudo apt install ansible
